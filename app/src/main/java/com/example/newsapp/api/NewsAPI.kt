3ad787cce2df21b695865bf6a8d5b8d1dbd17f0c
Axel Barlian <82171636+Zkavolo@users.noreply.github.com>
2024-12-31 17:18:02 +0700

    feat: linked newsApi using retrofit resp and instance

---
+++ app/src/main/java/com/example/newsapp/api/NewsAPI.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.api
+
+import com.bumptech.glide.load.Key
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getHeadlines(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "63507230e28f452da633038c38530b65"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
